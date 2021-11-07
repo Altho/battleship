7695c11820c9dfed06a0973fbca5927846b99ed7
Altho <alan.at.thomas@gmail.com>
2021-11-07 11:58:54 +0100

    board function

--- src/index.js
+++ src/index.js
@@ -1,30 +1,61 @@
 // import './style.css';
 
-const ship = (length,[...pos]) => {
+const ship = (length,orientation, [...pos]) => {
     let position = pos;
+    let shipOrientation = orientation;
     let shipLength = length;
     let isVertical = true;
     let isPlayer = true;
     let isSunk = false;
 
     let coordinates = () => {
-        if(isVertical===true){
-            let coord = []
 
-            for(let i = 0; i < shipLength; i++){
-                console.log('loop running');
-                let e = [];
-                e[0]= pos[0];
-                e[1]= pos[1]-i;
-                console.log(e);
-                coord.push(e);
+        let coord = []
+
+        for (let i = 0; i < shipLength; i++) {
+            let e = [];
+            if (shipOrientation === 'vertical') {
+                e[0] = pos[0];
+                e[1] = pos[1] - i;
+            } else {
+
+                e[0] = pos[0] + i;
+                e[1] = pos[1];
             }
-            return coord;
+
+            coord.push(e);
         }
+        return coord;
+
+
     }
-    return {position, shipLength,isVertical,isPlayer,isSunk,coordinates}
+    return {position, shipLength, isVertical, isPlayer, isSunk, coordinates}
+}
+
+const gameBoard = (size) => {
+    const gbSize = size;
+    const grid = () => {
+        const totalGrid = [];
+        const arr = [];
+        for(let i = 0; i < gbSize; i++){
+            for(let e = 0; e < gbSize; e++){
+              arr[0]=i;
+              arr[1]=e;
+              console.log('arr = '+ arr)
+
+            }
+        }
+        console.log('totalGrid = '+ totalGrid)
+
+        return totalGrid;
+    }
+    return{gbSize,grid};
 }
 
 const hitDetection = () => {
 }
-module.exports=ship;
+
+
+
+
+module.exports = {ship, gameBoard}
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,8 +1,26 @@
-const ship = require('../index');
+const mainMethod = require('../index');
+const ship = mainMethod.ship;
+const board = mainMethod.gameBoard;
 
-it('check position', () => {
-        const nostromo = ship(3,[5,5]);
+
+it('check position(vertical)', () => {
+        const nostromo = ship(3,'vertical', [5, 5]);
         expect(nostromo.coordinates())
-            .toStrictEqual([[5,5],[5,4],[5,3]]);
+            .toStrictEqual([[5, 5], [5, 4], [5, 3]]);
+    }
+)
+
+it('check position(horizontal)', () => {
+        const serenity = ship(5,'horizontal', [7, 4]);
+        serenity.isVertical = false;
+        expect(serenity.coordinates())
+            .toStrictEqual([[7, 4], [8, 4], [9, 4], [10, 4], [11, 4]]);
+    }
+)
+
+it('grid test)', () => {
+        const mainBoard = board(15);
+        expect(mainBoard.grid())
+            .toStrictEqual([[7, 4], [8, 4], [9, 4], [10, 4], [11, 4]]);
     }
 )
